7b042c67e3737fbc3eb5a05f14d83dc4969f6851
Sri Hari Kumar P <sriharikumar.0903@gmail.com>
2024-08-02 20:08:49 +0530

    index.js

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 $(document).ready(function(){
-    $(".enter").click(()=>{
+    var total = 0;
+    $(".enter").click(() => {
         let exp = $(".name").val();
         let cost = $(".number").val();
         let select = $(".select").val();
-        let  date = $(".date").val();
-        $("#first").after(`
+        let date = $(".date").val();
+        if(exp&&cost&&select&&date){
+        $("#first").after(`<tr class="item">
             <td>${exp}</td>
-            <td>${cost}</td>
+            <td>$${cost}</td>
             <td>${select}</td>
             <td>${date}</td>
-            <td><button>Delete</button></td>`); 
+            <td><button class="delete">Delete</button></td>
+        </tr>`); 
+        total += parseFloat(cost);
+        $("#val").html(`<h3>Total: $${total}</h3>`);
+    }
+    else{
+        alert("Fill all spaces");
+    };
+});
 
-        let total = total + parseFloat(cost);
         
+
+    $(document).on("click", ".delete", function() {
+        let cost = parseFloat($(this).closest(".item").find("td:nth-child(2)").text().substring(1));
+        total -= cost;
+        $("#val").html(`<h3>Total: $${total}</h3>`);
+        $(this).closest(".item").remove();
     });
-    
 });
